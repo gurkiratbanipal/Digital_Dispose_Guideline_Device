cd297e98ebc1acf1de4d6c4d2e5fae94da16fda2
Gurkirat Singh <55200846+gurkiratbanipal@users.noreply.github.com>
2020-04-08 23:29:13 -0400

    Update mainprogram.cpp

--- mainprogram.cpp
+++ mainprogram.cpp
@@ -28,22 +28,22 @@ int main()
 
 	 getline(cin, response);
 
-	     if (response == "Black")
+	     if (response == "black")
 	     {
 	    	 cout << "YES!! You are right"<< endl;
 	     }
-	     else if (response == "Green")
+	     else if (response == "green")
 	     {
 	    	 cout << "NO! Correct answer is Black Bin"<< endl;
 	     }
-	     else if (response == "Blue")
+	     else if (response == "blue")
 	     {
 	    	 cout << "NO! Correct answer is Black Bin"<< endl;
    	     }
 	     else
 	     {
 	    	cout << "You was supposed to write Black, Blue or Green"<< endl;
-	  		cout << "NO! Correct answer is Black Bin"<< endl;
+	  		cout << "Correct answer is Black Bin"<< endl;
 	     }
 
 //QUESTION#2//
@@ -54,48 +54,48 @@ int main()
 
 	getline(cin, response);
 
-		     if (response == "Blue")
+		     if (response == "blue")
 		     {
 		    	 cout << "YES!! You are right"<< endl;
 		     }
-		     else if (response == "Green")
+		     else if (response == "green")
 		     {
 		    	 cout << "NO! Correct answer is Blue Bin"<< endl;
 		     }
-		     else if (response == "Black")
+		     else if (response == "black")
 		     {
 		    	 cout << "NO! Correct answer is Blue Bin"<< endl;
 	   	     }
 		     else
 		     {
 		   		cout << "You was supposed to write Black, Blue or Green"<< endl;
-		   		cout << "NO! Correct answer is Blue Bin"<< endl;
+		   		cout << "Correct answer is Blue Bin"<< endl;
 		     }
 //QUESTION#3//
 
 	cout<< endl;
-    cout <<"Which Bin is Glass bottles belongs to? "<< endl;
+        cout <<"Which Bin is Glass bottles belongs to? "<< endl;
 	cout <<"Black, Blue or Green Bin?"<< endl;
 
  	getline(cin, response);
 
-	     if (response == "Blue")
+	     if (response == "blue")
    	     {
 	   	 cout << "YES!! You are right"<< endl;
 	   	 cout << "Even Broken Glass belongs to Blue Bin"<< endl;
          }
-	    else if (response == "Green")
+	    else if (response == "green")
 	     {
 	  	 cout << "NO! Correct answer is Blue Bin"<< endl;
 	     }
-	     else if (response == "Black")
+	     else if (response == "black")
 	     {
 	     cout << "NO! Correct answer is Blue Bin"<< endl;
 	     }
 	     else
 	     {
 	   	 cout << "You was supposed to write Black, Blue or Green"<< endl;
-	   	 cout << "NO! Correct answer is Blue Bin"<< endl;
+	   	 cout << "Correct answer is Blue Bin"<< endl;
 	     }
 //QUESTION#4//
 
@@ -105,20 +105,46 @@ int main()
 
  	getline(cin, response);
 
- 	if (response == "Green")
+ 	if (response == "green")
  	{
  		cout << "YES!! You are right"<< endl;
  	}
- 	else if (response == "Black")
+ 	else if (response == "black")
  	{
  		cout << "NO! Correct answer is Green Bin"<< endl;
  	}
- 	else if (response == "Blue")
+ 	else if (response == "blue")
  	{
  		cout << "NO! Correct answer is Green Bin"<< endl;
  	}
  	else
  	{
   		cout << "You was supposed to write Black, Blue or Green"<< endl;
-  		cout << "NO! Correct answer is Green Bin"<< endl;
+  		cout << "Correct answer is Green Bin"<< endl;
  	}
+//QUESTION#5//
+	
+   cout<< endl;
+   cout <<"Which Bin is Bubble Wrap belongs to? "<< endl;
+   cout <<"Black, Blue or Green Bin?"<< endl;
+
+ 	getline(cin, response);
+
+ 	if (response == "black")
+ 	{
+ 		cout << "YES!! You are right"<< endl;
+ 	}
+ 	else if (response == "green")
+ 	{
+ 		cout << "NO! Correct answer is Black Bin"<< endl;
+ 	}
+ 	else if (response == "blue")
+ 	{
+ 		cout << "NO! Correct answer is Black Bin"<< endl;
+ 	}
+  	else
+  	{
+  		cout << "You was supposed to write Black, Blue or Green"<< endl;
+  		cout << "Correct answer is Black Bin"<< endl;
+  	}
+
