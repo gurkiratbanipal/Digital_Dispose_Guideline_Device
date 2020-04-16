2526a0ca880eed8a430c8b9d2b5ecf5b2e0ff3f8
harpreet kaur <55203521+harpgill@users.noreply.github.com>
2020-04-16 00:52:16 -0400

    Update digdisgui.cpp

--- Software/Sources/digdisgui.cpp
+++ Software/Sources/digdisgui.cpp
@@ -1,1 +1,32 @@
- 
+ #include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
+
+DigDisGUI::DigDisGUI(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::DigDisGUI)
+{
+    ui->setupUi(this);
+}
+
+DigDisGUI::~DigDisGUI()
+{
+    delete ui;
+}
+
+
+void DigDisGUI::on_pushButton_2_clicked()
+{
+    QuizWin quizdialog;
+    quizdialog.setModal(true);
+    quizdialog.exec();
+}
+
+void DigDisGUI::on_pushButton_clicked()
+{
+    CalnWin calndialog;
+    calndialog.setModal(true);
+    calndialog.exec();
+}
+
+
