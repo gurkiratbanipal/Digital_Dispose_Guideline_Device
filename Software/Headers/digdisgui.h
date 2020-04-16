e7d73e9cd1bfc4d5ce2006d2c4db192429bd626b
harpreet kaur <55203521+harpgill@users.noreply.github.com>
2020-04-16 00:47:27 -0400

    Create digdisgui.h

---
+++ Software/Headers/digdisgui.h
@@ -0,0 +1,26 @@
+#ifndef DIGDISGUI_H
+#define DIGDISGUI_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class DigDisGUI; }
+QT_END_NAMESPACE
+
+class DigDisGUI : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    DigDisGUI(QWidget *parent = nullptr);
+    ~DigDisGUI();
+
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::DigDisGUI *ui;
+};
+#endif // DIGDISGUI_H
