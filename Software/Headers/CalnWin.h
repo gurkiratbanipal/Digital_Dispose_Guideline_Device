7769f70f1b2f61570f5ef4cc9723afd5413a4b43
Daman <55203353+damanpreetsd@users.noreply.github.com>
2020-04-16 00:52:12 -0400

    Updated CalnWin.h

--- Software/Headers/CalnWin.h
+++ Software/Headers/CalnWin.h
@@ -1,1 +1,22 @@
+#ifndef CALNWIN_H
+#define CALNWIN_H
 
+#include <QDialog>
+
+namespace Ui {
+class CalnWin;
+}
+
+class CalnWin : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CalnWin(QWidget *parent = nullptr);
+    ~CalnWin();
+
+private:
+    Ui::CalnWin *ui;
+};
+
+#endif // CALNWIN_H
