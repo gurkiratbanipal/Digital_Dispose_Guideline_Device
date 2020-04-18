52d907cd416dcc06e949111629af1661a378c26e
Daman <55203353+damanpreetsd@users.noreply.github.com>
2020-04-18 01:08:58 -0400

    Updated calnwin.h

--- Software/Daman/Headers/calnwin.h
+++ Software/Daman/Headers/calnwin.h
@@ -1,1 +1,28 @@
+#ifndef CALNWIN_H
+#define CALNWIN_H
+
+#include <QDialog>
+
+namespace Ui {
+class CalnWin;
+}
+
+class CalnWin : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CalnWin(QWidget *parent = nullptr);
+    ~CalnWin();
+
+private slots:
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_2_clicked();
+    
+    private:
+    Ui::CalnWin *ui;
+};
+
+#endif // CALNWIN_H
 
