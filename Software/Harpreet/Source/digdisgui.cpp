bbb517c6bb8691414e9b66cab88f008991546a29
harpreet kaur <55203521+harpgill@users.noreply.github.com>
2020-04-18 00:14:17 -0400

    Update digdisgui.cpp

--- Software/Harpreet/Source/digdisgui.cpp
+++ Software/Harpreet/Source/digdisgui.cpp
@@ -1,1 +1,43 @@
 
+#include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
+
+DigDisGUI::DigDisGUI(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::DigDisGUI)
+{
+    ui->setupUi(this);
+}
+
+DigDisGUI::~DigDisGUI()
+{
+    delete ui;
+}
+
+
+void DigDisGUI::on_pushButton_2_clicked()
+{
+    QuizWin quizdialog;
+    quizdialog.setModal(true);
+    quizdialog.exec();
+}
+
+void DigDisGUI::on_pushButton_clicked()
+{
+    CalnWin calndialog;
+    calndialog.setModal(true);
+    calndialog.exec();
+}
+{
+
+    QFile file("//home//harpgill//Documents//DigDisGUI2//Guidelines.txt");
+    if(!file.open(QIODevice::ReadOnly))
+        QMessageBox::information(0,"Book",file.errorString());
+
+    QTextStream in(&file);
+    ui->textBrowser->setText(in.readAll());
+
+}
+
+
