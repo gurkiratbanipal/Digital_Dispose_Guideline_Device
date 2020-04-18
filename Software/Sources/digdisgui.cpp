d138e906435255b6b0b06189c7725bd9a2119ac8
harpreet kaur <55203521+harpgill@users.noreply.github.com>
2020-04-17 23:58:22 -0400

    Update digdisgui.cpp

--- Software/Sources/digdisgui.cpp
+++ Software/Sources/digdisgui.cpp
@@ -28,5 +28,14 @@ void DigDisGUI::on_pushButton_clicked()
     calndialog.setModal(true);
     calndialog.exec();
 }
+{
 
+    QFile file("//home//harpgill//Documents//DigDisGUI2//Guidelines.txt");
+    if(!file.open(QIODevice::ReadOnly))
+        QMessageBox::information(0,"Book",file.errorString());
+
+    QTextStream in(&file);
+    ui->textBrowser->setText(in.readAll());
+
+}
 
