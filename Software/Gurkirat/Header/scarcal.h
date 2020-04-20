6598fe90fbc7577b8c6e92d6e910e24ee3d27f5b
Gurkirat Singh <55200846+gurkiratbanipal@users.noreply.github.com>
2020-04-20 03:07:13 -0400

    Update scarcal.h

--- Software/Gurkirat/Header/scarcal.h
+++ Software/Gurkirat/Header/scarcal.h
@@ -1,1 +1,25 @@
 
+#ifndef SCARCAL_H
+#define SCARCAL_H
+
+#include <QDialog>
+#include <QScrollArea>
+
+namespace Ui {
+class Scarcal;
+}
+
+class Scarcal : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Scarcal(QWidget *parent = nullptr);
+    ~Scarcal();
+
+private:
+    Ui::Scarcal *ui;
+
+};
+
+#endif // SCARCAL_H
