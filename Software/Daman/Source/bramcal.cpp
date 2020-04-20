4b5dbc24c578321725933b84daa606d93c096ba0
Daman <55203353+damanpreetsd@users.noreply.github.com>
2020-04-20 03:11:59 -0400

    Update bramcal.cpp

--- Software/Daman/Source/bramcal.cpp
+++ Software/Daman/Source/bramcal.cpp
@@ -1,1 +1,17 @@
 
+#include "bramcal.h"
+#include "ui_bramcal.h"
+#include <QPixmap>
+#include <QLabel>
+
+Bramcal::Bramcal(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Bramcal)
+{
+    ui->setupUi(this);
+}
+
+Bramcal::~Bramcal()
+{
+    delete ui;
+}
