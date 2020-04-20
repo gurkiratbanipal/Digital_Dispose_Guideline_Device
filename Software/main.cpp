dea89f536f6fa1e2f47fc71b7056591cd78aae6c
Gurkirat Singh <55200846+gurkiratbanipal@users.noreply.github.com>
2020-04-20 03:03:37 -0400

    Update main.cpp

--- Software/main.cpp
+++ Software/main.cpp
@@ -1,1 +1,12 @@
 
+#include "digdisgui.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DigDisGUI w;
+    w.show();
+    return a.exec();
+}
