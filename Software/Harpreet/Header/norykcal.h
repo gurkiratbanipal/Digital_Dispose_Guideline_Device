a4991f7d5f0c1f47f4af0464fc0287984eff00f0
harpreet kaur <55203521+harpgill@users.noreply.github.com>
2020-04-20 03:19:33 -0400

    Update norykcal.h

--- Software/Harpreet/Header/norykcal.h
+++ Software/Harpreet/Header/norykcal.h
@@ -1,1 +1,23 @@
 
+#ifndef NORYKCAL_H
+#define NORYKCAL_H
+
+#include <QDialog>
+
+namespace Ui {
+class norykcal;
+}
+
+class norykcal : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit norykcal(QWidget *parent = nullptr);
+    ~norykcal();
+
+private:
+    Ui::norykcal *ui;
+};
+
+#endif // NORYKCAL_H
