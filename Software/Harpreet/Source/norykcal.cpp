4ee0cc5ef7e07835c3c3b4ae86e3dbfbb11e68d2
harpreet kaur <55203521+harpgill@users.noreply.github.com>
2020-04-20 03:16:40 -0400

    Update norykcal.cpp

--- Software/Harpreet/Source/norykcal.cpp
+++ Software/Harpreet/Source/norykcal.cpp
@@ -1,1 +1,17 @@
 
+#include "norykcal.h"
+#include "ui_norykcal.h"
+#include <QPixmap>
+#include <QLabel>
+
+norykcal::norykcal(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::norykcal)
+{
+    ui->setupUi(this);    
+}
+
+norykcal::~norykcal()
+{
+    delete ui;
+}
