f3fa103bf82e3b23fd4576665122b896d3f380e9
Gurkirat Singh <55200846+gurkiratbanipal@users.noreply.github.com>
2020-04-20 03:05:55 -0400

    Update scarcal.cpp

--- Software/Gurkirat/Source/scarcal.cpp
+++ Software/Gurkirat/Source/scarcal.cpp
@@ -1,1 +1,17 @@
 
+#include "scarcal.h"
+#include "ui_scarcal.h"
+#include <QLabel>
+#include <QPixmap>
+
+Scarcal::Scarcal(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Scarcal)
+{
+    ui->setupUi(this);
+}
+
+Scarcal::~Scarcal()
+{
+    delete ui;
+}
