d7678df26dc8cfbbc482038d902f55dbfae7a956
Daman <55203353+damanpreetsd@users.noreply.github.com>
2020-04-20 03:13:15 -0400

    Update bramcal.h

--- Software/Daman/Headers/bramcal.h
+++ Software/Daman/Headers/bramcal.h
@@ -1,1 +1,23 @@
 
+#ifndef BRAMCAL_H
+#define BRAMCAL_H
+
+#include <QDialog>
+
+namespace Ui {
+class Bramcal;
+}
+
+class Bramcal : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Bramcal(QWidget *parent = nullptr);
+    ~Bramcal();
+
+private:
+    Ui::Bramcal *ui;
+};
+
+#endif // BRAMCAL_H
