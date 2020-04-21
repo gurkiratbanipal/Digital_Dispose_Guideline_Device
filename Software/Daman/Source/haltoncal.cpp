a2c4b1ee9db21b65123f488efd68d322671277fe
Daman <55203353+damanpreetsd@users.noreply.github.com>
2020-04-21 02:25:32 -0400

    Update haltoncal.cpp

--- Software/Daman/Source/haltoncal.cpp
+++ Software/Daman/Source/haltoncal.cpp
@@ -1,1 +1,20 @@
 
+#include "haltoncal.h"
+#include "ui_haltoncal.h"
+
+HaltonCal::HaltonCal(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::HaltonCal)
+{
+    ui->setupUi(this);
+    QPixmap pix("/home/pi/Downloads/Halton.png");
+    ui->label->setPixmap(pix.scaled(4000,4000,Qt::KeepAspectRatio));
+
+
+
+}
+
+HaltonCal::~HaltonCal()
+{
+    delete ui;
+}
