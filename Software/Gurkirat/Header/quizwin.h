bef4205656e261fd9c08975feb62cf9850583d25
Gurkirat Singh <55200846+gurkiratbanipal@users.noreply.github.com>
2020-04-21 01:51:47 -0400

    Update quizwin.h

--- Software/Gurkirat/Header/quizwin.h
+++ Software/Gurkirat/Header/quizwin.h
@@ -25,6 +25,33 @@ private slots:
 
     void on_pushButton_4_clicked();
 
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_16_clicked();
+
+    void on_pushButton_17_clicked();
+
+    void on_pushButton_18_clicked();
+
+    void on_pushButton_19_clicked();
+
+    void on_pushButton_20_clicked();
 
 
 
