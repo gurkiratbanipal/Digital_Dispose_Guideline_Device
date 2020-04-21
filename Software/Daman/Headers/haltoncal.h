a8d8f244f8e9d94f9ba9c5919f9e727257b07bbd
Daman <55203353+damanpreetsd@users.noreply.github.com>
2020-04-21 02:14:43 -0400

    Update haltoncal.h

--- Software/Daman/Headers/haltoncal.h
+++ Software/Daman/Headers/haltoncal.h
@@ -1,1 +1,23 @@
 
+#ifndef HALTONCAL_H
+#define HALTONCAL_H
+
+#include <QDialog>
+
+namespace Ui {
+class HaltonCal;
+}
+
+class HaltonCal : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit HaltonCal(QWidget *parent = nullptr);
+    ~HaltonCal();
+
+private:
+    Ui::HaltonCal *ui;
+};
+
+#endif // HALTONCAL_H
