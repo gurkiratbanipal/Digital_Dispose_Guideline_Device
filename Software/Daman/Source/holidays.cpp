5c8874378909ad910951a11c9a6aa8ea06a840fd
Daman <55203353+damanpreetsd@users.noreply.github.com>
2020-04-27 03:46:32 -0400

    Update holidays.cpp

--- Software/Daman/Source/holidays.cpp
+++ Software/Daman/Source/holidays.cpp
@@ -12,6 +12,18 @@ HoliDays::HoliDays(QWidget *parent) :
     ui->setupUi(this);
     QPixmap pix("/home/pi/Downloads/FinalQT/Holiday.png");
     ui->label->setPixmap(pix.scaled(1200,750,Qt::KeepAspectRatio));
+        
+    QPixmap bin ("/home/pi/Downloads/Black.png");
+    ui->GarbageBin->setPixmap(bin.scaled(65,80,Qt::KeepAspectRatio));
+
+    QPixmap bin2 ("/home/pi/Downloads/Recycle.png");
+    ui->GarbageBin2->setPixmap(bin2.scaled(65,80,Qt::KeepAspectRatio));
+
+    QPixmap bin3 ("/home/pi/Downloads/green.png");
+    ui->GarbageBin3->setPixmap(bin3.scaled(65,80,Qt::KeepAspectRatio));
+
+    QPixmap bin4 ("/home/pi/Downloads/Grass.png");
+    ui->GarbageBin4->setPixmap(bin4.scaled(55,80,Qt::KeepAspectRatio));     
 }
 
 HoliDays::~HoliDays()
